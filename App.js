3ab576de566796f8fe834039501ba67941e46aad
Roopam <roopamr2411@gmail.com>
2024-02-25 01:37:26 +0530

    create a simple form useing react native

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
-import {View,Text, TextInput, Button } from 'react-native';
+import {View,Text,TextInput,Button} from 'react-native';
 import styles from './AppStyle';
 
 const App = () => {
-  const [name,setName] = useState("")
-  const handelOnPress = () =>{
-    setName("Hello Roopam")
+  const [name,setName] = useState("");
+  const [password,setPassword] = useState("");
+  const [displayName,setDisplayName] = useState("");
+  const [displayPassword,setDisplayPassword] = useState("")
+  const handelOnSignUp = ()=>{
+    setDisplayName(name),
+    setName(""),
+    setPassword(""),
+    setDisplayPassword(password)
+  };
+  const hadelClear = ()=>{
+    setDisplayName("")
+    setDisplayPassword("")
   }
   return (
-    <View style={styles.box}>
-      <Text style={styles.name}>Your Name:{name}</Text>
-      <TextInput style={styles.input} onChangeText={(text)=>setName(text)} placeholder='Enter Your Name' value={name}/>
-      <Button title='submit' onPress={()=>setName("")}/>
+    <View style={styles.main}>
+      <Text style={styles.heading}>Sing Up</Text>
+      <Text>username:{displayName},Password:{displayPassword}</Text>
+      <View style={styles.form}>
+      <View style={styles.usernameSection}>
+        <Text style={styles.userheading}>Username</Text>
+        <TextInput style={styles.userinput} onChangeText={(text)=>setName(text)} value={name}/>
+      </View>
+      <View style={styles.usernameSection}>
+        <Text style={styles.userheading}>Password</Text>
+        <TextInput style={styles.userinput} secureTextEntry={true} onChangeText={(text)=>setPassword(text)} value={password}/>
+      </View>
+      </View>
+      <View style={styles.btn}>
+      <Button title='Sign Up' style={styles.btn} onPress={handelOnSignUp}/>
+      <Button title='Clear' style={styles.btn} onPress={hadelClear}/>
+      </View>
     </View>
   )
 }
--- AppStyle.js
+++ AppStyle.js
@@ -1,26 +1,55 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    box:{
-        display:"flex",
-        justifyContent:"center",
-        alignContent:"center",
-        backgroundColor:'green',
+    main:{
         height:1000,
-        width:500,
-        textAlign:"center",
-        paddingLeft:10,
+        width:400,
+        backgroundColor:"black",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        gap:40,
+        backgroundColor:"white",
+        position:"relative",
+        top:-200
     },
-    name:{
-        fontWeight:"900",
+    heading:{
+        color:"black",
         fontSize:30,
-        color:"white"
+        fontWeight:"700",
+        position:"relative",
+        left:-20
     },
-    input:{
-        borderColor:"white",
-        borderWidth:2,
-        width:300
+    usernameSection:{
+        display:"flex",
+        flexDirection:"column",
+        gap:10
+    },
+    userheading:{
+        display:"flex",
+        color:"black",
+        textAlign:"left",
+        fontSize:25
+    },
+    userinput:{
+        color:"black",
+        borderWidth:1,
+        borderColor:"black",
+        width:200,
+        borderTopRightRadius:10,
+        borderBottomLeftRadius:10
+    },
+    form:{
+        display:"flex",
+        gap:10
+    },
+    btn:{
+        display:"flex",
+        justifyContent:"space-between",
+        flexDirection:"row",
+        gap:20,
     }
-})
+
+});
 
 export default styles;
