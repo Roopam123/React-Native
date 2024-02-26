e9d9de52d680edc563fe8f7cdbadf1478fd8f252
Roopam <roopamr2411@gmail.com>
2024-02-26 21:38:53 +0530

    map in react native

--- App.js
+++ App.js
@@ -1,41 +1,19 @@
-import React, { useState } from 'react';
-import {View,Text,TextInput,Button} from 'react-native';
-import styles from './AppStyle';
+import React from 'react';
+import {View,Text,ScrollView} from 'react-native';
+import data from './data.json'
 
 const App = () => {
-  const [name,setName] = useState("");
-  const [password,setPassword] = useState("");
-  const [displayName,setDisplayName] = useState("");
-  const [displayPassword,setDisplayPassword] = useState("")
-  const handelOnSignUp = ()=>{
-    setDisplayName(name),
-    setName(""),
-    setPassword(""),
-    setDisplayPassword(password)
-  };
-  const hadelClear = ()=>{
-    setDisplayName("")
-    setDisplayPassword("")
-  }
   return (
-    <View style={styles.main}>
-      <Text style={styles.heading}>Sing Up</Text>
-      <Text>username:{displayName},Password:{displayPassword}</Text>
-      <View style={styles.form}>
-      <View style={styles.usernameSection}>
-        <Text style={styles.userheading}>Username</Text>
-        <TextInput style={styles.userinput} onChangeText={(text)=>setName(text)} value={name}/>
-      </View>
-      <View style={styles.usernameSection}>
-        <Text style={styles.userheading}>Password</Text>
-        <TextInput style={styles.userinput} secureTextEntry={true} onChangeText={(text)=>setPassword(text)} value={password}/>
-      </View>
-      </View>
-      <View style={styles.btn}>
-      <Button title='Sign Up' style={styles.btn} onPress={handelOnSignUp}/>
-      <Button title='Clear' style={styles.btn} onPress={hadelClear}/>
-      </View>
-    </View>
+    <ScrollView>
+      {data.map((item,index)=>(
+        <View key={index} style={{backgroundColor:"green",marginBottom:3}}>
+          <Text style={{color:"red"}}>{item.name}</Text>
+          <Text>{item.email}</Text>
+          <Text>{item.phone}</Text>
+          <Text>{item.address}</Text>
+        </View>
+      ))}
+    </ScrollView>
   )
 }
 
