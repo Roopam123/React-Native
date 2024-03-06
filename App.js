a88528f7905620034166b555f8bea54d9e85af81
Roopam <roopamr2411@gmail.com>
2024-03-06 15:45:09 +0530

    Presseble in react native (33)

--- App.js
+++ App.js
@@ -1,54 +1,40 @@
-import { View, Text,Modal, TouchableHighlight,StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text,Pressable,StyleSheet } from 'react-native'
+import React from 'react'
 
 const App = () => {
-  const [modal,setModal] = useState(false)
   return (
     <View style={styles.main}>
-      {/* Modal basically take a three value 1.transparent={true} 2. visible={true} 3.  animationType='slide'*/}
-      <Modal transparent={true}
-            visible={modal}
-            animationType='slide'
-      >
-        <View style={styles.modal}>
-        <View>
-          <Text style={styles.modalText}>Hello! Roopam</Text>
-        </View>
-        </View>
-      </Modal>
-      <TouchableHighlight style={styles.btn} onPress={()=>setModal(false)}>
-        <Text style={styles.btnText}>Show Modal</Text>
-      </TouchableHighlight>
+      <Pressable  
+      // onPress={()=>{console.warn("on Press")}}
+      // onLongPress={()=>{console.warn("On Long Press")}}
+      onPressIn={()=>console.warn("On press in")}
+      onPressOut={()=>console.warn("On press out")}
+      style={styles.pressable}>
+        <Text style={styles.btn}>Button</Text>
+      </Pressable>
     </View>
   )
 }
-
 const styles = StyleSheet.create({
   main:{
     flex:1,
-    alignItems:"center",
-    justifyContent:"flex-end"
-  },
-  btn:{
-    backgroundColor:"black",
-    width:"90%",
-    marginBottom:20,
-    padding:15,
-    borderRadius:10
-  },
-  btnText:{
-    color:"white",
-    textAlign:"center",
-    fontWeight:"700",
-    fontSize:20
-  },
-  modal:{
-    display:"flex",
     justifyContent:"center",
     alignItems:"center",
-    backgroundColor:"gray",
-    flex:.9
+  },
+  pressable:{
+    backgroundColor:"black",
+    width:200,
+    padding:10,
+    borderRadius:20,
+    shadowColor:"red",
+    elevation:1
+  },
+  btn:{
+    textAlign:"center",
+    fontSize:20,
+    color:"white",
+    fontWeight:"800"
   }
 })
 
-export default App
+export default App;
