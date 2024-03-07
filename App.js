83edf1cf445993227db11388105dd0043a782a23
Roopam <roopamr2411@gmail.com>
2024-03-08 00:09:18 +0530

    navigation in react native

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { View, Text,Platform } from 'react-native'
-import React from 'react'
+import React, { Component } from 'react'
+import { Button, Text, View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from "@react-navigation/stack"
 
-const App = () => {
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='login' component={Login}/>
+          <Stack.Screen name='home' component={Home}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+}
+
+const Home = ()=>{
   return (
     <View>
-      <Text>My Platform:{Platform.OS}</Text>
-      <Text>Version:{Platform.Version}</Text>
+      <Text>Home</Text>
+    </View>
+  )
+}
+const Login = (props)=>{
+  return(
+    <View>
+      <Text>Login</Text>
+      <Button title='go to home page' onPress={()=>{props.navigation.navigate("home")}}/>
     </View>
   )
 }
