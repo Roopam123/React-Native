1a371304588ba18408b306bb3baaf2a1ac4780c6
Roopam <roopamr2411@gmail.com>
2024-03-08 01:08:13 +0530

    Tab Navigation in react native

--- App.js
+++ App.js
@@ -1,44 +1,35 @@
 import React, { Component } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from "@react-navigation/stack"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
-const Stack = createStackNavigator();
-
+const Tab = createBottomTabNavigator();
 export class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Login' component={Login} 
-          options={{
-            header:()=><Text>Hello</Text>,
-            headerRight:()=><Button title='Right'/>,
-            title:"Login Page",
-            headerTintColor:"green",
-            headerStyle:{
-              backgroundColor:"black"
-            }
-          }}/>
-          <Stack.Screen name='Home' component={Home}/>
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name='login' component={Login}/>
+          <Tab.Screen name='signup' component={SignUp}/>
+        </Tab.Navigator>
       </NavigationContainer>
     )
   }
 }
 
-const Home = ()=>{
-  return (
-    <View>
-    </View>
-  )
-}
-const Login = (props)=>{
+const Login = ()=>{
   return(
     <View>
-      <Button title='go to home page' onPress={()=>{props.navigation.navigate("Home")}}/>
+    <Text>Login</Text>
     </View>
   )
 }
 
+const SignUp = ()=>{
+  return(
+    <View>
+      <Text>SignUp</Text>
+    </View>
+  )
+}
 export default App
