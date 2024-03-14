7986ae2ffd5031f72b1ff84abb89900b22936bde
Roopam <roopamr2411@gmail.com>
2024-03-14 11:22:45 +0530

    "use of useRef"

--- App.js
+++ App.js
@@ -1,36 +1,77 @@
-import { View, Text,ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState,useRef } from 'react';
 
 const App = () => {
-  const [data,setData] = useState([]);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const nameInfo = useRef();
 
-  useEffect(()=>{
-    getApiData();
-  },[])
-
-  const getApiData = async ()=>{
-    try {
-      const url = "https://jsonplaceholder.typicode.com/posts";
-      let fetchData = await fetch(url);
-      let apiData = await fetchData.json();
-      if (apiData) {
-        setData(apiData)
-      }
-    } catch (error) {
-      console.warn("Error on the api call function",error)
-    }
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.warn(name, lastName, password);
+    nameInfo.current.focus();
+    nameInfo.current.setNativeProps({
+      fontSize:20,
+      color:"green"
+    })
+    setName("")
+    setLastName("")
+    setPassword("")
   }
+
   return (
-    <ScrollView>
-      <Text style={{textAlign:"center",fontSize:25,color:"green"}}>API Data</Text>
-      {data.map((ele,i)=>
-      <View key={i} style={{backgroundColor:"#ccc",borderWidth:1,borderColor:"red",padding:20,margin:10}}>
-        <Text>Id: - {ele.id}</Text>
-        <Text>Title: - {ele.title}</Text>
-        <Text>Body: - {ele.body}</Text>
-      </View>)}
-    </ScrollView>
-  )
+    <View style={styles.main}>
+      <Text style={styles.mainText}>App Form</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        ref={nameInfo}
+        onChangeText={text => setName(text)}
+        placeholder='Enter Your Name'
+      />
+      <TextInput
+        style={styles.input}
+        value={lastName}
+        onChangeText={text => setLastName(text)}
+        placeholder='Enter Your Last Name'
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry={true}
+        placeholder='Enter Your Password'
+      />
+      <Button title='Submit' onPress={submitForm}/>
+    </View>
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // React Native does not support gap in StyleSheet. You will need to add margins to individual elements or use other layout views.
+  },
+  mainText: {
+    fontSize: 30,
+    fontWeight: "800",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: "90%",
+    color: "black",
+    paddingLeft: 20,
+    height: 40,
+    borderRadius: 10,
+    marginTop: 20, // Added to separate inputs and button
+  },
+  btn:{
+    marginTop:30
+  }
+});
+
+export default App;
